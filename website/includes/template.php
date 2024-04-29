4c8585bd9bf8d5bbf9e1768a78fa9787ef701027
AjaySayer <0852351@schoolsnet.act.edu.au>
2024-04-30 09:38:56 +1000

    remove stuff

--- website/includes/template.php
+++ website/includes/template.php
@@ -61,9 +61,6 @@
         echo '
         <ul class="navbar-nav ms-auto"> <!--Right side of navbar-->
             <li class="nav-item active">
-                <a class="nav-link text-white" href="' . BASE_URL .'/assets/videos/video.mp4">Video</a></a>
-            </li>
-            <li class="nav-item active">
                 <a class="nav-link text-white" href="' . BASE_URL .'pages/contactUs/contact.php">Contact&nbsp;us</a>
             </li>
             <li class="nav-item active">
